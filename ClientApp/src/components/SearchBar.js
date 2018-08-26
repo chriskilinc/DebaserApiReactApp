138311286d2eb93b7fcbcc3cc02e6fe997a0bfe2
chriskilinc <chriskilinc@gmail.com>
2018-08-26 17:28:49 +0200

    Added Inputs and OnChange functions for SearchBar. Component

--- ClientApp/src/components/SearchBar.js
+++ ClientApp/src/components/SearchBar.js
@@ -2,10 +2,68 @@
 import '../css/SearchBar.css';
 
 export default class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dateFrom: "2018-01-01",
+            dateTo: "2018-01-01",
+            venues: [
+                {
+                    id: "medis",
+                    name: "Medborgarplatsen"
+                },
+                {
+                    id: "slussen",
+                    name: "Slussen"
+                }
+            ]
+        }
+    }
+
+    componentDidMount = () => {
+        var dateObj = new Date();
+        this.setState({
+            dateFrom: new Date().toISOString().split('T')[0],
+            dateTo: new Date().toISOString().split('T')[0]
+        })
+    }
+
+    handleFromInput = event => {
+        this.setState({
+            dateFrom: event.target.value
+        });
+    };
+
+    handleToInput = event => {
+        this.setState({
+            dateTo: event.target.value
+        });
+    };
+
     render() {
         return (
             <section className="searchbar">
-                <p>searchbar</p>
+                <div className="option">
+                    <p>From:</p>
+                    <input type="date" value={this.state.dateFrom} onChange={this.handleFromInput} />
+                </div>
+
+                <div className="option">
+                    <p>To:</p>
+                    <input type="date" value={this.state.dateTo} onChange={this.handleToInput} />
+                </div>
+
+                <div className="option">
+                    <p>Venue:</p>
+
+                    <select>
+                        {this.state.venues.map(venue => <option key={venue.id} value={venue.id}>{venue.name}</option>)}
+                    </select>
+                </div>
+
+                
+                
             </section>
         );
     }
