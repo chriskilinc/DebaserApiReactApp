e2c82c5da28fd690cfc1c532b8fa0c0ffa8af28b
chriskilinc <chriskilinc@gmail.com>
2018-08-26 23:34:06 +0200

    Implemented Map() of all fetched Events as well as simple error handling

--- ClientApp/src/components/EventsContainer.js
+++ ClientApp/src/components/EventsContainer.js
@@ -3,18 +3,21 @@ import React, { Component } from 'react';
 import Event from '../components/Event';
 
 export default class EventsContainer extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
     render() {
         return (
             <div className="eventscontainer">
-                <p>PÅGÅENDE EVENEMANG</p>
+                {this.props.events.length > 0 ? <p>PÅGÅENDE EVENEMANG</p> : <p>SÖK EVENT</p>}
+                
                 <div className="events">
-                    <Event />
-                    <Event />
-                    <Event />
-                    <Event />
-                    <Event />
-                    <Event />
-                    <Event />
+
+                    {this.props.eventsNotFound ? <p className="events--error">Tyvärr finns det ej några evenemang under denna tidsperiod, försök att söka under en annan tidsperiod!</p> : null}
+                    {this.props.events.length > 0 ? this.props.events.map(event => <Event key={event.EventId} eventName={event.Event} subHead={event.SubHead} venue={event.Venue} eventDate={event.EventDate} room={event.Room} age={event.Age} time={event.Open} eventUrl={event.EventUrl} />) : null}
+
+
 
                 </div>
                 
