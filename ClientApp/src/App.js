f5b533e020fed7d3cfc214226ab42b5f37afa8d4
chriskilinc <chriskilinc@gmail.com>
2018-08-26 16:51:33 +0200

    Added and implemented SearchBar Component

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 import React, { Component } from 'react';
 import Header from './components/Header';
+import SerchBar from './components/SearchBar';
 
 export default class App extends Component {
   render() {
       return (
           <div className="application">
-            <Header/>
+              <Header />
+              <SearchBar />
             <p>Hello world</p>
         </div>
     );
---
+++ ClientApp/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+import '../css/SearchBar.css';
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <section className="searchbar">
+                <p>searchbar</p>
+            </section>
+        );
+    }
+}
