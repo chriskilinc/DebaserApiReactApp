c1af394cd6a438ba571eaef78daff63da1a3e5fd
chriskilinc <chriskilinc@gmail.com>
2018-08-26 20:03:40 +0200

    Added function 'handleSearch'

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,14 +2,39 @@ import './index.css';
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import EventsContainer from './components/EventsContainer';
 
 export default class App extends Component {
-  render() {
-      return (
-          <div className="application">
-            <Header />
-            <SearchBar />
-        </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                
+            ]
+        }
+    }
+
+    fetchDebaserApi = (dateTo, dateFrom, venue) => {
+        console.log(dateTo, dateFrom, venue);
+
+        //  Build Fetch
+
+        //  Fetch
+        var fetch = "fetch";
+
+    }
+
+    handleSearch = (searchOptions) => {
+        console.log(searchOptions);
+    }
+
+    render() {
+        return (
+            <div className="application">
+                <Header />
+                <SearchBar handleSearch={this.handleSearch} />
+              <EventsContainer />
+          </div>
+      );
+    }
 }
