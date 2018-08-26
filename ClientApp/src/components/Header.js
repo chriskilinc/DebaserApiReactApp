10b494f70be2bee924431cb2c0a1f57f0c1b62ae
chriskilinc <chriskilinc@gmail.com>
2018-08-26 16:06:03 +0200

    Added Header Component

---
+++ ClientApp/src/components/Header.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <p>Nav</p>
+            </nav>
+        );
+    }
+}
