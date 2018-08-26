0cb8eb7619a4bd36d1678b1f7154f5b22222d116
chriskilinc <chriskilinc@gmail.com>
2018-08-26 20:02:40 +0200

    Added Event Component and Styling

---
+++ ClientApp/src/components/Event.js
@@ -0,0 +1,15 @@
+﻿import '../css/Event.css';
+import React, { Component } from 'react';
+
+export default class Event extends Component {
+    render() {
+        return (
+            <div className="event">
+                <p className="event--header">2018-01-19 / Medis, Stora Scenen</p>
+                <p className="event--status">SLUTSÅLD</p>
+                <a href="#" className="event--title">The Xx + New Look</a>
+                <p>Tid: 00-03 / Ålder: 20 år</p>
+            </div>
+        );
+    }
+}
