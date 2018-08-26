f5f0f891aa3190959a7dcb976d3493fb00f4c51d
chriskilinc <chriskilinc@gmail.com>
2018-08-26 20:02:24 +0200

    Added EventsContainer Component and styling

---
+++ ClientApp/src/components/EventsContainer.js
@@ -0,0 +1,24 @@
+﻿import '../css/EventsContainer.css';
+import React, { Component } from 'react';
+import Event from '../components/Event';
+
+export default class EventsContainer extends Component {
+    render() {
+        return (
+            <div className="eventscontainer">
+                <p>PÅGÅENDE EVENEMANG</p>
+                <div className="events">
+                    <Event />
+                    <Event />
+                    <Event />
+                    <Event />
+                    <Event />
+                    <Event />
+                    <Event />
+
+                </div>
+                
+            </div>
+        );
+    }
+}
