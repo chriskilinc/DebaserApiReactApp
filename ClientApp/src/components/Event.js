c7eca442738089c6b7cf46f87f3aacb6617d7089
chriskilinc <chriskilinc@gmail.com>
2018-08-26 23:31:54 +0200

    Styled Event Component

--- ClientApp/src/components/Event.js
+++ ClientApp/src/components/Event.js
@@ -5,10 +5,10 @@ export default class Event extends Component {
     render() {
         return (
             <div className="event">
-                <p className="event--header">2018-01-19 / Medis, Stora Scenen</p>
-                <p className="event--status">SLUTSÅLD</p>
-                <a href="#" className="event--title">The Xx + New Look</a>
-                <p>Tid: 00-03 / Ålder: 20 år</p>
+                <p className="event--header">{this.props.eventDate} / {this.props.venue}, {this.props.room}</p>
+                <p className="event--status">{this.props.status}</p>
+                <a href={this.props.eventUrl} className="event--title">{this.props.eventName}  {this.props.subHead}</a>
+                <p>Tid: {this.props.time} / Ålder: {this.props.age}</p>
             </div>
         );
     }
