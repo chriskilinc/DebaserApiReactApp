a1efd7f652c7ea3d119f7ea15e0eef8ed681655c
chriskilinc <chriskilinc@gmail.com>
2018-08-26 16:40:43 +0200

    Styling App.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,11 @@
+import './index.css';
 import React, { Component } from 'react';
 import Header from './components/Header';
 
 export default class App extends Component {
   render() {
-    return (
-        <div>
+      return (
+          <div className="application">
             <Header/>
             <p>Hello world</p>
         </div>
